b906185c0e0f3ba039de1327e97647f97ab2196d
elhispanopolako <tadeo98hiszpan@gmail.com>
2021-12-29 10:24:43 +0100

    add agechecker

---
+++ src/test/java/AgeChecker.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class AgeChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj swój wiek");
+        int age = scanner.nextInt();
+        if (age <= 0) {
+            System.out.println("Jeszcze Cie nie ma swiecie");
+        }
+        else if (age < 18) {
+            System.out.println("Jesteś niepełnoletni");
+        }
+        else {
+            System.out.println("Jesteś pełnoletni");
+        }
+    }
+}
